8e92dfc39b19fa8235a0776bdd717d7062b55165
dwikboss <dwikyvanbosstraten@livewallgroup.com>
2024-03-06 14:24:39 +0100

    initial commit

---
+++ frontend/src/components/Model.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="model">
+        <div ref="container">
+            <canvas ref="canvasRef"></canvas>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { ARButton } from "three/examples/jsm/webxr/ARButton";
+import * as THREE from 'three';
+
+export default defineComponent({
+    name: 'ModelComponent',
+    mounted() {
+        const scene = new THREE.Scene();
+        const canvasRef = this.$refs.canvasRef;
+
+        const camera = new THREE.PerspectiveCamera(75, canvasRef.clientWidth / canvasRef.clientHeight, 0.1, 1000);
+        camera.position.z = 2;
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        const renderer = new THREE.WebGLRenderer({ 
+            canvas: canvasRef,
+            antialias: true,
+            alpha: true
+        });
+        renderer.setSize(canvasRef.clientWidth, canvasRef.clientHeight);
+        renderer.xr.enabeled = true;
+
+        document.body.appendChild(ARButton.createButton(renderer));
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+        animate();
+    },
+});
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page home">
+    <div class="full-width">
+      <h1>Mixed Reality</h1>
+      <ModelComponent />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ModelComponent from '@/components/Model.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    ModelComponent,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.page.home {
+  background-color: var(--black);
+  height: 100vh;
+
+  .full-width {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  h1 {
+    margin-top: 32px;
+    margin-bottom: 16px;
+    color: var(--white);
+    text-align: center;
+  }
+
+  h2 {
+    color: var(--white);
+    font-weight: 200;
+  }
+}
+</style>
