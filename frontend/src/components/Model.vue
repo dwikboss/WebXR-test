951bcf16c337568d877bfc4ff7402fc284903b5b
dwikboss <dwikyvanbosstraten@livewallgroup.com>
2024-03-14 13:40:12 +0100

    test

--- frontend/src/components/Model.vue
+++ frontend/src/components/Model.vue
@@ -7,72 +7,141 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ARButton } from "three/examples/jsm/webxr/ARButton";
 import * as THREE from 'three';
 
 export default defineComponent({
     name: 'ModelComponent',
+    data() {
+        return {
+            imageCounter: 0 as number,
+        }
+    },
     mounted() {
+        let reticle;
+        let renderer;
+        let controls;
+        let hitTestSource = null;
+        let hitTestSourceRequested = false;
+
         const scene = new THREE.Scene();
         const canvasRef = this.$refs.canvasRef;
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100) as any;
         camera.position.y = 0;
         camera.position.z = 2;
         camera.lookAt(new THREE.Vector3(0, 0, 0));
+        scene.add(camera);
 
-        const renderer = new THREE.WebGLRenderer({
-            canvas: canvasRef as HTMLCanvasElement,
-            antialias: true,
-            alpha: true
-        }) as any;
+        reticle = new THREE.Mesh(
+            new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+            new THREE.MeshStandardMaterial()
+        );
+        reticle.matrixAutoUpdate = false;
+        reticle.visible = false;
+        scene.add(reticle);
 
-        const arButton = ARButton.createButton(renderer) as any;
-        document.body.appendChild(arButton);
-        const session = renderer.xr.setSession('immersive-ar', {
-            requiredFeatures: ['hit-test']
-        });
+        let loop = (timestamp, frame) => {
+            if (frame) {
+                const referenceSpace = renderer.xr.getReferenceSpace();
+                let session = renderer.xr.getSession();
 
-        const raycaster = new THREE.Raycaster();
-        const intersection = new THREE.Vector3();
+                if (hitTestSourceRequested === false) {
+                    session.requestReferenceSpace("viewer").then((referenceSpace) => {
+                        session
+                            .requestHitTestSource({ space: referenceSpace })
+                            .then((source) => {
+                                hitTestSource = source;
+                            });
+                    });
 
-        let controller = renderer.xr.getController(0);
-        controller.addEventListener("select", onSelect);
-        console.log(controller);
+                    hitTestSourceRequested = true;
 
-        function onSelect() {
-            placePainting();
-        }
-        function placePainting() {
-            const textureLoader = new THREE.TextureLoader();
-            const texture = textureLoader.load('/images/ivewave.jpg');
+                    session.addEventListener("end", () => {
+                        hitTestSourceRequested = false;
+                        hitTestSource = null;
+                    });
+                }
 
-            const geometry = new THREE.PlaneGeometry(1, 0.5);
-            const material = new THREE.MeshBasicMaterial({ map: texture });
-
-            const painting = new THREE.Mesh(geometry, material);
-            painting.position.set(0, 0, -2);
-            scene.add(painting);
-        }
+                if (hitTestSource) {
+                    const hitTestResults = frame.getHitTestResults(hitTestSource);
+                    if (hitTestResults.length > 0) {
+                        const hit = hitTestResults[0];
+                        reticle.visible = true;
+                        reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+                    } else {
+                        reticle.visible = false;
+                    }
+                }
+            }
+            controls.update();
+            renderer.render(scene, camera);
+        };
 
         let resizeCallback = () => {
             renderer.setSize(window.innerWidth, window.innerHeight);
+
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
         };
 
+        renderer = new THREE.WebGLRenderer({
+            canvas: canvasRef as HTMLCanvasElement,
+            antialias: true,
+            alpha: true
+        });
         renderer.setSize(window.innerWidth - 200, window.innerHeight - 200);
         renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.render(scene, camera);
-        (renderer as any).xr.enabled = true;
+        renderer.xr.enabled = true;
 
-        const animate = () => {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-        };
-        animate();
+        renderer.setAnimationLoop(loop);
+
+        document.body.appendChild(
+            ARButton.createButton(renderer as any, {
+                requiredFeatures: ["hit-test"],
+            })
+        );
+
+        controls = new OrbitControls(camera, canvasRef);
+        controls.enableDamping = true;
+
+        window.addEventListener("resize", resizeCallback);
+        placePainting();
+
+        function placePainting() {
+            if (reticle.visible) {
+                const textureLoader = new THREE.TextureLoader();
+                const texture = textureLoader.load('/images/ivewave.jpg');
+
+                const geometry = new THREE.PlaneGeometry(1, 0.5);
+                const material = new THREE.MeshBasicMaterial({ map: texture });
+
+                const painting = new THREE.Mesh(geometry, material);
+                painting.position.setFromMatrixPosition(reticle.matrix);
+                this.imageCounter++;
+                scene.add(painting);
+                // if (this.imageCounter == 0) {
+
+                // } else if (this.imageCounter == 1) {
+                //     const textureLoader = new THREE.TextureLoader();
+                //     const texture = textureLoader.load('/images/ivegroup.jpg');
+
+                //     const geometry = new THREE.PlaneGeometry(0.6, 1);
+                //     const material = new THREE.MeshBasicMaterial({ map: texture });
+
+                //     const painting = new THREE.Mesh(geometry, material);
+                //     painting.position.setFromMatrixPosition(reticle.matrix);
+                //     this.imageCounter++;
+                //     scene.add(painting);
+                // }
+            }
+            // console.log(this.imageCounter);
+        }
+
+        let controller = renderer.xr.getController(0);
+        controller.addEventListener("select", placePainting);
     },
 });
 
